fb87f1415b1efcdba1fdca9a4fea456114584c8e
s-full <60407568+s-full@users.noreply.github.com>
2023-05-12 18:16:15 -0700

    MC chain now working, need to fix updaters

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 from functools import partial
-
 import geopandas as gpd
 import gerrychain
 import gerrychain as gc
-
-
 import matplotlib.pyplot as plt
 from gerrychain import (GeographicPartition, Partition, Graph, MarkovChain,
                         proposals, updaters, constraints, accept, Election)
 from gerrychain.proposals import recom
 from functools import partial
-import pandas
+import pandas as pd
 from gerrychain import Graph, Partition, MarkovChain, proposals, accept, constraints, GeographicPartition, updaters, graph
 from gerrychain.constraints import within_percent_of_ideal_population, single_flip_contiguous
 from gerrychain.proposals import recom
@@ -22,7 +19,7 @@ import matplotlib.pyplot as plt
 df = gpd.read_file("AlamedaCountyPopCorrected")
 
 # check coord system
-#print(df.crs)
+# print(df.crs)
 
 # convert coord system
 df_utm = df.to_crs("EPSG:32610")
@@ -36,9 +33,12 @@ graph = gc.Graph.from_geodataframe(df_utm)
 df_utm["Population"] = df_utm["Population"].str.replace(",", "")
 df_utm["Population"] = df_utm["Population"].astype(int)
 
-graph.add_data(df_utm, columns=["Population"])
+total_pop = df_utm["Population"].sum()
 
-# create copy to filter out tracts of 0 population
+graph.add_data(df_utm, columns=["Population"])
+graph.add_data(df_utm, columns=["NAME"])
+
+# create copy to filter out tracts of 0 population, not working
 # df_filtered = df_utm.copy()
 # df_filtered = df_filtered[df_filtered["Population"] > 0]
 
@@ -47,49 +47,41 @@ districts = 7
 steps = 1000
 compactness_bound = 0.01
 
-# proposal = recom(graph, districts, pop_col="Population",
-#                  pop_target=graph.total_population / districts,
-#                  epsilon=compactness_bound, node_repeats=1,
-#                  spanning_tree=False, spanning_forest=False, tree_crossover=False, spanning_tree_random_iters=0,
-#                  spanning_forest_distortion_threshold=None, spanning_tree_distortion_threshold=None, **kwargs)
+init_partition = Partition(graph, "NAME", {"population": updaters.Tally("Population", alias="population")})
+pop_updater = gerrychain.updaters.Tally("population", alias="population")
 
 
-init_partition = Partition(graph, "Population", {"population": updaters.Tally("Population", alias="population")})
+constraints = [
+    # within_percent_of_ideal_population(init_partition, 0.1),
+    single_flip_contiguous,
+]
 
-# constraints = [
-#     within_percent_of_ideal_population(init_partition, 0.1),
-#     single_flip_contiguous,
-# ]
+plan = Partition(init_partition.graph,"NAME")
 population_target = sum(init_partition["population"].values()) / districts
 proposal = gc.proposals.propose_random_flip
 chain = MarkovChain(
     proposal=proposal,
-    constraints=[],
+    constraints=constraints,
     accept=accept.always_accept,
     initial_state=init_partition,
-    total_steps=1000,
+    total_steps=50,
 )
-
-# colors = ["red", "blue", "green", "purple", "orange", "yellow", "brown"]
 #
-# # Loop over the chain and plot each partition
-# for partition in chain:
-#     new_partition = partition
-#     new_graph = graph.rebuild
-#     gdf = gpd.GeoDataFrame.from_features(partition.graph.nodes.values())
-#     partition.plot(gdf, cmap="tab20", edgecolor="black", alpha=0.5, figsize=(10, 10))
-#     plt.axis("off")
-#     plt.show()
-# data = pandas.DataFrame(
-#     sorted(partition["population"].values())
-#     for partition in chain
-# )
+# # colors = ["red", "blue", "green", "purple", "orange", "yellow", "brown"]
+# #
+# # # Loop over the chain and plot each partition
+for partition in chain:
+    plan = Partition(partition.graph, "NAME")
+    plan.plot(df_utm, figsize=(10, 10), cmap="tab20")
+    plt.axis('off')
+    plt.show()
 
 
-
-print(population_target)
-
-plan = Partition(graph, "Population")
+#
+#
+# print(population_target)
+#
+plan = Partition(graph, "NAME")
 plan.plot(df_utm, figsize=(10, 10), cmap="tab20")
 plt.axis('off')
 plt.show()
